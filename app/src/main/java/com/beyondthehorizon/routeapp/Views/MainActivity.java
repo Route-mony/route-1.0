fc8cd9d30f335faf1f1649c7d2b866df90266da5
kelvinwm <kelvinwmblogs@gmail.com>
2019-11-18 08:34:00 +0300

    chore(folder-structure) setup project
    setup the basic project file structure
    [Maintains #169728546]
    	# with '#' will be ignored, and an empty message aborts the commit.

--- app/src/main/java/com/beyondthehorizon/routeapp/Views/MainActivity.java
+++ app/src/main/java/com/beyondthehorizon/routeapp/Views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.beyondthehorizon.routeapp;
+package com.beyondthehorizon.routeapp.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.beyondthehorizon.routeapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
