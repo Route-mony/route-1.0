28a5f57046e6c61ba621738c65e0d8dec8d26eed
pmutondo12@gmail.com <pmutondo12@gmail.com>
2019-11-30 23:25:28 +0300

    Implement verification for complete signup screen.

---
+++ app/src/main/java/com/beyondthehorizon/routeapp/Views/SignupVerifiedActivity.java
@@ -0,0 +1,15 @@
+package com.beyondthehorizon.routeapp.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.beyondthehorizon.routeapp.R;
+
+public class SignupVerifiedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_verified);
+    }
+}
