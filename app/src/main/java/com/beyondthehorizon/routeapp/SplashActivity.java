f3d03e1402c8ea91ef50bd8760b1b573e0d49285
pmutondo12@gmail.com <pmutondo12@gmail.com>
2019-12-08 14:13:20 +0300

    Implement home screen layout

--- app/src/main/java/com/beyondthehorizon/routeapp/SplashActivity.java
+++ app/src/main/java/com/beyondthehorizon/routeapp/SplashActivity.java
@@ -9,21 +9,21 @@ import android.os.Handler;
 import com.beyondthehorizon.routeapp.Views.ServicesActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
 
-
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Do something after 5s = 5000ms
-                startActivity(new Intent(SplashActivity.this, ServicesActivity.class));
-                finish();
-            }
-        }, 1000);
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                // Do something after 5s = 5000ms
+//                startActivity(new Intent(SplashActivity.this, ServicesActivity.class));
+//                finish();
+//            }
+//        }, 1000);
     }
+
+
 }
