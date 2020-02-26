c06ee32c13d8bf50d1a35f41c57a641fa76a69c0
kelvinwm <kelvinwmblogs@gmail.com>
2020-02-26 18:36:10 +0300

    paybill screens

---
+++ app/src/main/java/com/beyondthehorizon/routeapp/paybillpayments/BuyGoodsActivity.kt
@@ -0,0 +1,16 @@
+package com.beyondthehorizon.routeapp.paybillpayments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.beyondthehorizon.routeapp.R
+
+class BuyGoodsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_goods)
+    }
+
+    fun prevPage(view: View) {}
+}
