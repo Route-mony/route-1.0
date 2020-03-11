57fdfb9550a259930b960ad29969e79b3f2c89bd
PromasterGuru <pmutondo12@gmail.com>
2020-03-12 00:55:58 +0300

    Add Mpesa integration to route via Interswitch

--- app/src/main/java/com/beyondthehorizon/routeapp/views/FundAmountActivity.kt
+++ app/src/main/java/com/beyondthehorizon/routeapp/views/FundAmountActivity.kt
@@ -9,16 +9,18 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.beyondthehorizon.routeapp.R
 import com.beyondthehorizon.routeapp.databinding.ActivityFundAmountBinding
 import com.beyondthehorizon.routeapp.utils.Constants.*
 import com.beyondthehorizon.routeapp.utils.CustomProgressBar
+import com.google.android.material.snackbar.Snackbar
 import com.interswitchgroup.mobpaylib.MobPay
-import com.interswitchgroup.mobpaylib.model.Card
-import com.interswitchgroup.mobpaylib.model.Customer
-import com.interswitchgroup.mobpaylib.model.Merchant
-import com.interswitchgroup.mobpaylib.model.Payment
+import com.interswitchgroup.mobpaylib.api.model.TransactionResponse
+import com.interswitchgroup.mobpaylib.interfaces.TransactionFailureCallback
+import com.interswitchgroup.mobpaylib.interfaces.TransactionSuccessCallback
+import com.interswitchgroup.mobpaylib.model.*
 import kotlinx.android.synthetic.main.enter_pin_transaction_pin.view.*
 import java.text.DecimalFormat
 import java.text.NumberFormat
@@ -106,6 +108,29 @@ class FundAmountActivity : AppCompatActivity() {
             }
 
             binding.btnRequest.setOnClickListener {
+                val lower = 100000000
+                val upper = 999999999
+                var merchantId = "TZLETU0001"
+                var country = oldIntent.getStringExtra(COUNTRY)
+                var domain = "ISWKE"
+                var amount = amount
+                var transactionRef = ((Math.random() * (upper - lower)).toInt() + lower).toString()
+                var terminalId = "3TLP0001"
+                var currency = "KES"
+                var orderId = "TZL_ANDR_132"
+                var preauth = "1"
+                var customerId = prefs.getString(USER_ID, "")
+                var customerEmail = prefs.getString(USER_EMAIL, "")
+                var clientId = "IKIAAB09E4ECCB92A508AEDE0D01B36472E27C309F05"
+                var clientSecret = "f4Vm0o0bXBQTIG4DAKrbs4urZZgJSieih/EiHjnf9/E="
+
+
+                var merchant = Merchant(merchantId, domain);
+                var payment = Payment(amount, transactionRef, "MOBILE", terminalId, "CRD", currency, orderId);
+                payment.setPreauth(preauth);
+                var customer = Customer(customerId);
+                customer.setEmail(customerEmail);
+
                 if (binding.txtAmount.text.isNullOrEmpty()) {
                     Toast.makeText(this, "Please enter amount to request", Toast.LENGTH_LONG).show()
                 } else if (amount.toInt() <= 0) {
@@ -121,32 +146,12 @@ class FundAmountActivity : AppCompatActivity() {
                 } else if (transactionType!!.compareTo(SEND_MONEY) == 0) {
                     showSendMoneyDialog()
                 } else if (transactionType!!.compareTo(LOAD_WALLET_FROM_CARD) == 0) {
-                    val lower = 100000000
-                    val upper = 999999999
-                    var merchantId = "TZLETU0001"
                     var expDate = oldIntent.getStringExtra(EXPIRY_DATE)
-                    var expYear = expDate.substring(0, 2)
-                    var expMonth = expDate.substring(3, 5)
+                    var expYear = expDate.substring(3, 5)
+                    var expMonth = expDate.substring(0, 2)
                     var cvvNumber = oldIntent.getStringExtra(CVV_NUMBER)
-                    var country = oldIntent.getStringExtra(COUNTRY)
-                    var domain = "ISWKE"
-                    var amount = amount
-                    var transactionRef = ((Math.random() * (upper - lower)).toInt() + lower).toString()
-                    var terminalId = "3TLP0001"
-                    var currency = "KES"
-                    var orderId = "TZL_ANDR_132"
-                    var preauth = "1"
-                    var customerId = prefs.getString(USER_ID, "")
-                    var customerEmail = prefs.getString(USER_EMAIL, "")
-                    var clientId = "IKIAAB09E4ECCB92A508AEDE0D01B36472E27C309F05"
-                    var clientSecret = "f4Vm0o0bXBQTIG4DAKrbs4urZZgJSieih/EiHjnf9/E="
 
                     try {
-                        var merchant = Merchant(merchantId, domain);
-                        var payment = Payment(amount, transactionRef, "MOBILE", terminalId, "CRD", currency, orderId);
-                        payment.setPreauth(preauth);
-                        var customer = Customer(customerId);
-                        customer.setEmail(customerEmail);
                         var card = Card(merchantId, cvvNumber, expYear, expMonth);
                         lateinit var mobPay: MobPay;
 
@@ -157,13 +162,12 @@ class FundAmountActivity : AppCompatActivity() {
                                 card,
                                 merchant,
                                 payment,
-                                customer,
-                                {
+                                customer,{
                                     Log.d("INTERSWITCH_MESSAGE", it.transactionOrderId)
                                     Toast.makeText(this@FundAmountActivity, it.transactionOrderId, Toast.LENGTH_LONG).show()
                                 }, {
-                            Log.d("INTERSWITCH_MESSAGE", it.message)
-                            Toast.makeText(this@FundAmountActivity, it.message, Toast.LENGTH_LONG).show()
+                                Log.d("INTERSWITCH_MESSAGE", it.message)
+                                Toast.makeText(this@FundAmountActivity, it.message, Toast.LENGTH_LONG).show()
 
                         });
                     } catch (e: Exception) {
@@ -171,7 +175,27 @@ class FundAmountActivity : AppCompatActivity() {
                         Toast.makeText(this@FundAmountActivity, e.message, Toast.LENGTH_LONG).show();
                     }
                 } else if (transactionType!!.compareTo(LOAD_WALLET_FROM_MPESA) == 0) {
-                    var mobileNumber = prefs.getString(PHONE_NUMBER, "")
+                    var mobileNumber = oldIntent.getStringExtra(PHONE_NUMBER)
+                    var mobile = Mobile(mobileNumber, Mobile.Type.MPESA)
+                    var mobPay: MobPay
+                    try {
+                        mobPay = MobPay.getInstance(this@FundAmountActivity, clientId, clientSecret, null)
+                    } catch (e: Exception) {
+                        Toast.makeText(this@FundAmountActivity, e.message, Toast.LENGTH_LONG).show()
+                        return@setOnClickListener
+                    }
+                    mobPay.makeMobileMoneyPayment(
+                            mobile,
+                            merchant,
+                            payment,
+                            customer,{
+                                    Log.d("INTERSWITCH_MESSAGE", "Transaction succeeded, ref:\t${it.transactionOrderId}")
+                                    Toast.makeText(this@FundAmountActivity, "Transaction succeeded, ref:\t${it.transactionOrderId}", Toast.LENGTH_LONG).show()
+                            },{
+                        Log.d("INTERSWITCH_MESSAGE", it.message)
+                        Toast.makeText(this@FundAmountActivity, it.message, Toast.LENGTH_LONG).show()
+
+                    })
                 }
 
             }
