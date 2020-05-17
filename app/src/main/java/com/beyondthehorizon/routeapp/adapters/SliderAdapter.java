e9adef2f5f0fe83b8e0c2612a2d0ff0bc340aa71
PromasterGuru <pmutondo12@gmail.com>
2020-05-17 23:47:19 +0300

    Implements get adverts feature and updates load requests feature to adjust limit

--- app/src/main/java/com/beyondthehorizon/routeapp/adapters/SliderAdapter.java
+++ app/src/main/java/com/beyondthehorizon/routeapp/adapters/SliderAdapter.java
@@ -3,26 +3,26 @@ package com.beyondthehorizon.routeapp.adapters;
 import android.view.View;
 import android.widget.TextView;
 
-import com.beyondthehorizon.routeapp.models.Movies;
 import com.beyondthehorizon.routeapp.R;
+import com.beyondthehorizon.routeapp.models.Adverts;
 import com.github.islamkhsh.CardSliderAdapter;
 
 import java.util.ArrayList;
 
-public class SliderAdapter extends CardSliderAdapter<Movies> {
+public class SliderAdapter extends CardSliderAdapter<Adverts> {
 
-    public SliderAdapter(ArrayList<Movies> movies) {
+    public SliderAdapter(ArrayList<Adverts> movies) {
         super(movies);
     }
 
     @Override
-    public void bindView(int position, View itemContentView, Movies item) {
+    public void bindView(int position, View itemContentView, Adverts item) {
         //TODO bind item object with item layout view
         // add items to arraylist
         TextView title = itemContentView.findViewById(R.id.ioTitle);
         TextView desc = itemContentView.findViewById(R.id.ioDesc);
-        title.setText(item.getName());
-        desc.setText(item.getExplanation());
+        title.setText(item.getTitle());
+        desc.setText(item.getDescription());
 
     }
 
