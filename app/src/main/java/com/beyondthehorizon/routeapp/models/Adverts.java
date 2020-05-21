a86118718a43c2af3d0914c776372953d877000d
PromasterGuru <pmutondo12@gmail.com>
2020-05-21 18:20:25 +0300

    Display adverts with gif images

--- app/src/main/java/com/beyondthehorizon/routeapp/models/Adverts.java
+++ app/src/main/java/com/beyondthehorizon/routeapp/models/Adverts.java
@@ -1,11 +1,32 @@
 package com.beyondthehorizon.routeapp.models;
+
+import android.net.Uri;
+
 public class Adverts {
     String title;
     String description;
+    Uri icon_url;
 
-    public Adverts(String name, String explanation) {
+    public Adverts(Uri url, String name, String explanation) {
         this.title = name;
         this.description = explanation;
+        this.icon_url = url;
+    }
+
+    public Uri getIcon_url() {
+        return icon_url;
+    }
+
+    public void setIcon_url(Uri icon_url) {
+        this.icon_url = icon_url;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getTitle() {
