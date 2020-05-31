409088fcd37b6541b55e9ad4153bf49abf2e32b1
kelvinwm <kelvinwmblogs@gmail.com>
2020-06-01 01:46:00 +0300

    edit beneficiary

--- app/src/main/java/com/beyondthehorizon/routeapp/adapters/MultiChoiceContactsAdapter.java
+++ app/src/main/java/com/beyondthehorizon/routeapp/adapters/MultiChoiceContactsAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,11 +38,13 @@ public class MultiChoiceContactsAdapter extends RecyclerView.Adapter<MultiChoice
     List<MultiContactModel> list;
     List<MultiContactModel> chosenContacts = new ArrayList<>();
     SharedPreferences prefs;
+    SparseBooleanArray itemStateArray = new SparseBooleanArray();
 
     public MultiChoiceContactsAdapter(Context context, List<MultiContactModel> list) {
         this.context = context;
         this.list = list;
         prefs = context.getSharedPreferences(Constants.REG_APP_PREFERENCES, 0);
+        itemStateArray.clear();
     }
 
     @Override
@@ -55,86 +58,89 @@ public class MultiChoiceContactsAdapter extends RecyclerView.Adapter<MultiChoice
     @Override
     public void onBindViewHolder(final ContactHolder holder, final int position) {
 
-        final MultiContactModel contact = list.get(position);
 //        final MultiContactModel contact = list.get(position);
-//
-        Log.e("BABA223", "onBindViewHolder: " + contact.is_route());
+        holder.bind(position);
+////        final MultiContactModel contact = list.get(position);
+//        Log.e("BABA223", "onBindViewHolder: " + contact.is_route());
         holder.checkBox.setEnabled(false);
-        holder.checkBox.setChecked(list.get(position).is_selected());
-        holder.username.setText(contact.getUsername());
-        holder.single_contact.setText(contact.getPhone_number());
-        holder.checkBox.setTag(position);
+//        holder.checkBox.setChecked(list.get(position).is_selected());
+//        holder.username.setText(contact.getUsername());
+//        holder.single_contact.setText(contact.getPhone_number());
+//        holder.checkBox.setTag(position);
+//
+//        if (holder.checkBox.isChecked()) {
+//            holder.rowLayout.setBackgroundColor(Color.parseColor("#D5D9DB"));
+////            holder.username.setTextColor(Color.parseColor("#1D1F29"));
+////            holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
+//        } else {
+//            holder.rowLayout.setBackgroundColor(Color.parseColor("#EDEEF0"));
+////            holder.username.setTextColor(Color.parseColor("#1D1F29"));
+////            holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
+//        }
+//
+//        RequestOptions requestOptions = new RequestOptions();
+//        requestOptions = requestOptions.transforms(new CenterCrop(), new RoundedCorners(16));
+//        if (contact.is_route()) {
+//            Glide.with(context)
+//                    .load(contact.getImage())
+//                    .centerCrop()
+//                    .error(R.drawable.group416)
+//                    .placeholder(R.drawable.group416)
+//                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+//                    .skipMemoryCache(true)
+//                    .apply(requestOptions)
+//                    .into(holder.circleImageView);
+//        } else {
+//            Glide.with(context)
+//                    .load(contact.getImage())
+//                    .centerCrop()
+//                    .error(R.drawable.default_avatar)
+//                    .placeholder(R.drawable.default_avatar)
+//                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+//                    .skipMemoryCache(true)
+//                    .apply(requestOptions)
+//                    .into(holder.circleImageView);
+//        }
+//        holder.rowLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                holder.checkBox.performClick();
+////                Integer pos = (Integer) holder.checkBox.getTag();
+////                notifyDataSetChanged();
+////                if (list.get(pos).is_selected()) {
+////                    holder.rowLayout.setBackgroundColor(Color.parseColor("#ffffff"));
+//////                    holder.username.setTextColor(Color.parseColor("#1D1F29"));
+//////                    holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
+////                    list.get(pos).set_selected(false);
+////                } else {
+////                    list.get(pos).set_selected(true);
+////                    holder.rowLayout.setBackgroundColor(Color.parseColor("#889AA6"));
+//////                    holder.username.setTextColor(Color.parseColor("#1D1F29"));
+//////                    holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
+////                }
+////                chosenContacts.clear();
+////                for (int j = 0; j < list.size(); j++) {
+////
+////                    if (list.get(j).is_selected()) {
+//                        chosenContacts.add(new MultiContactModel(
+//                                list.get(j).getUsername(),
+//                                list.get(j).getPhone_number(),
+//                                list.get(j).getImage(),
+//                                "",
+//                                list.get(j).is_route(),
+//                                true
+//                        ));
+////                        holder.rowLayout.setBackgroundColor(Color.parseColor("#567845"));
+//////                        holder.username.setTextColor(Color.parseColor("#1D1F29"));
+//////                        holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
+////                    }
+////
 
-        if (holder.checkBox.isChecked()) {
-            holder.rowLayout.setBackgroundColor(Color.parseColor("#D5D9DB"));
-//            holder.username.setTextColor(Color.parseColor("#1D1F29"));
-//            holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
-        } else {
-            holder.rowLayout.setBackgroundColor(Color.parseColor("#EDEEF0"));
-//            holder.username.setTextColor(Color.parseColor("#1D1F29"));
-//            holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
-        }
-
-        RequestOptions requestOptions = new RequestOptions();
-        requestOptions = requestOptions.transforms(new CenterCrop(), new RoundedCorners(16));
-        if (contact.is_route()) {
-            Glide.with(context)
-                    .load(contact.getImage())
-                    .centerCrop()
-                    .error(R.drawable.group416)
-                    .placeholder(R.drawable.group416)
-                    .diskCacheStrategy(DiskCacheStrategy.NONE)
-                    .skipMemoryCache(true)
-                    .apply(requestOptions)
-                    .into(holder.circleImageView);
-        } else {
-            Glide.with(context)
-                    .load(contact.getImage())
-                    .centerCrop()
-                    .error(R.drawable.default_avatar)
-                    .placeholder(R.drawable.default_avatar)
-                    .diskCacheStrategy(DiskCacheStrategy.NONE)
-                    .skipMemoryCache(true)
-                    .apply(requestOptions)
-                    .into(holder.circleImageView);
-        }
-        holder.rowLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                holder.checkBox.performClick();
-                Integer pos = (Integer) holder.checkBox.getTag();
-                notifyDataSetChanged();
-                if (list.get(pos).is_selected()) {
-                    holder.rowLayout.setBackgroundColor(Color.parseColor("#ffffff"));
-//                    holder.username.setTextColor(Color.parseColor("#1D1F29"));
-//                    holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
-                    list.get(pos).set_selected(false);
-                } else {
-                    list.get(pos).set_selected(true);
-                    holder.rowLayout.setBackgroundColor(Color.parseColor("#889AA6"));
-//                    holder.username.setTextColor(Color.parseColor("#1D1F29"));
-//                    holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
-                }
-                chosenContacts.clear();
-                for (int j = 0; j < list.size(); j++) {
-
-                    if (list.get(j).is_selected()) {
-                        chosenContacts.add(new MultiContactModel(
-                                list.get(j).getUsername(),
-                                list.get(j).getPhone_number(),
-                                list.get(j).getImage(),
-                                "",
-                                list.get(j).is_route(),
-                                true
-                        ));
-                        holder.rowLayout.setBackgroundColor(Color.parseColor("#567845"));
-//                        holder.username.setTextColor(Color.parseColor("#1D1F29"));
-//                        holder.single_contact.setTextColor(Color.parseColor("#1D1F29"));
-                    }
-                }
-
-            }
-        });
+////                }
+//
+//
+//            }
+//        });
     }
 
     @Override
@@ -142,12 +148,15 @@ public class MultiChoiceContactsAdapter extends RecyclerView.Adapter<MultiChoice
         return list.size();
     }
 
-    public static class ContactHolder extends RecyclerView.ViewHolder {
+    public class ContactHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView username, single_contact;
         CheckBox checkBox;
         CircleImageView circleImageView;
         LinearLayout rowLayout;
+
+        final SharedPreferences.Editor editor = prefs.edit();
+        final Gson gsonn = new Gson();
 
         public ContactHolder(View itemView) {
             super(itemView);
@@ -157,6 +166,86 @@ public class MultiChoiceContactsAdapter extends RecyclerView.Adapter<MultiChoice
             single_contact = itemView.findViewById(R.id.contact);
             checkBox = itemView.findViewById(R.id.checkBox_select);
             circleImageView = itemView.findViewById(R.id.profile_image);
+
+            itemView.setOnClickListener(this);
+        }
+
+        void bind(int position) {
+            // use the sparse boolean array to check
+            final MultiContactModel contact = list.get(position);
+
+//            checkBox.setChecked(list.get(position).is_selected());
+            username.setText(contact.getUsername());
+            single_contact.setText(contact.getPhone_number());
+
+            if (!itemStateArray.get(position, false)) {
+                checkBox.setChecked(false);
+                rowLayout.setBackgroundColor(Color.parseColor("#EDEEF0"));
+            } else {
+                checkBox.setChecked(true);
+                rowLayout.setBackgroundColor(Color.parseColor("#D5D9DB"));
+            }
+            RequestOptions requestOptions = new RequestOptions();
+            requestOptions = requestOptions.transforms(new CenterCrop(), new RoundedCorners(16));
+            if (contact.is_route()) {
+                Glide.with(context)
+                        .load(contact.getImage())
+                        .centerCrop()
+                        .error(R.drawable.group416)
+                        .placeholder(R.drawable.group416)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
+                        .apply(requestOptions)
+                        .into(circleImageView);
+            } else {
+                Glide.with(context)
+                        .load(contact.getImage())
+                        .centerCrop()
+                        .error(R.drawable.default_avatar)
+                        .placeholder(R.drawable.default_avatar)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
+                        .apply(requestOptions)
+                        .into(circleImageView);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+
+            Log.e("AMADEADMAN", "bind: " + adapterPosition);
+
+            if (!itemStateArray.get(adapterPosition, false)) {
+                checkBox.setChecked(true);
+                itemStateArray.put(adapterPosition, true);
+                rowLayout.setBackgroundColor(Color.parseColor("#D5D9DB"));
+                chosenContacts.add(new MultiContactModel(
+                        list.get(adapterPosition).getUsername(),
+                        list.get(adapterPosition).getPhone_number(),
+                        list.get(adapterPosition).getImage(),
+                        "0",
+                        list.get(adapterPosition).is_route(),
+                        true
+                ));
+
+            } else {
+                checkBox.setChecked(false);
+                itemStateArray.put(adapterPosition, false);
+                rowLayout.setBackgroundColor(Color.parseColor("#EDEEF0"));
+                chosenContacts.remove(new MultiContactModel(
+                        list.get(adapterPosition).getUsername(),
+                        list.get(adapterPosition).getPhone_number(),
+                        list.get(adapterPosition).getImage(),
+                        "0",
+                        list.get(adapterPosition).is_route(),
+                        true
+                ));
+            }
+
+            String jsonn = gsonn.toJson(chosenContacts);
+            editor.putString(Constants.MY_MULTI_CHOICE_SELECTED_CONTACTS, jsonn);
+            editor.apply();
         }
     }
 
--- app/src/main/java/com/beyondthehorizon/routeapp/views/multicontactschoice/ui/main/SendToManyActivity.kt
+++ app/src/main/java/com/beyondthehorizon/routeapp/views/multicontactschoice/ui/main/SendToManyActivity.kt
@@ -1,30 +1,113 @@
 package com.beyondthehorizon.routeapp.views.multicontactschoice.ui.main
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.beyondthehorizon.routeapp.R
+import com.beyondthehorizon.routeapp.adapters.GroupSendToManyAdapter
 import com.beyondthehorizon.routeapp.bottomsheets.EditSendToManyBottomSheet
 import com.beyondthehorizon.routeapp.models.BulkyRequestModel
+import com.beyondthehorizon.routeapp.models.MultiContactModel
+import com.beyondthehorizon.routeapp.utils.Constants
+import com.beyondthehorizon.routeapp.utils.Constants.MY_MULTI_CHOICE_SELECTED_CONTACTS
+import com.beyondthehorizon.routeapp.utils.Constants.MY_ROUTE_CONTACTS_NEW
+import com.beyondthehorizon.routeapp.views.FundRequestedActivity
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_send_to_many.*
+import org.json.JSONArray
+import java.lang.reflect.Type
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class SendToManyActivity : AppCompatActivity(), EditSendToManyBottomSheet.EditSendToManyBottomSheetListener {
+class SendToManyActivity : AppCompatActivity(), EditSendToManyBottomSheet.EditSendToManyBottomSheetListener, GroupSendToManyAdapter.SendToManyInterface {
 
+    var arrayList = ArrayList<MultiContactModel>()
+    val arrayListJson = ArrayList<String>()
+    lateinit var usersAdapter: GroupSendToManyAdapter
+    private lateinit var editor: SharedPreferences.Editor
+    private lateinit var prefs: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_to_many)
+        prefs = getSharedPreferences(Constants.REG_APP_PREFERENCES, 0)
+        editor = prefs.edit()
 
-        addFab.setOnClickListener {
-            val editSendToManyBottomSheet = EditSendToManyBottomSheet();
-            editSendToManyBottomSheet.show(supportFragmentManager, "Edit Contact");
+        bankButton.setOnClickListener {
+            val message = "You have successfully send ${totalAmount.text.toString()} to them.(sample reply)"
+            val intent = Intent(this, FundRequestedActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.putExtra("Message", message)
+            startActivity(intent)
+            finish()
         }
+
+
+        usersAdapter = GroupSendToManyAdapter(this, this) { position ->
+            run {
+                var amountTotal = 0
+                if (arrayList.size > 1) {
+//                    val newArray = Arrays.copyOfRange(arrayList, 1, arrayList.size - 1);
+                    for (bulkyRequestModel: MultiContactModel in arrayList.subList(1, arrayList.size)) {
+//                        amountTotal += bulkyRequestModel.amount.toInt() * bulkyRequestModel.quantity.toInt()
+                    }
+//                    totals.text = amountTotal.toString()
+//                    totalCard.visibility = View.VISIBLE
+//                    emptyList.visibility = View.GONE
+                } else {
+//                    totalCard.visibility = View.GONE
+//                    emptyList.visibility = View.VISIBLE
+                }
+
+            }
+        }
+//
+        bulkRequestRecycler.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = usersAdapter
+        }
+        arrayList.add(MultiContactModel("Name"
+                , "Quantity", "Quantity", "Amount", is_route = false, is_selected = false))
+        usersAdapter.setContact(arrayList)
+
+        sendToManyFunction()
+    }
+
+    private fun sendToManyFunction() {
+        val gson = Gson()
+        val type: Type = object : TypeToken<ArrayList<MultiContactModel>>() {}.type
+        arrayList = gson.fromJson(prefs.getString(MY_MULTI_CHOICE_SELECTED_CONTACTS, ""), type)
+
+        Log.e("NOWNOW", prefs.getString(MY_MULTI_CHOICE_SELECTED_CONTACTS, ""))
+        usersAdapter.setContact(arrayList)
     }
 
     fun prevPage(view: View) {
         onBackPressed()
     }
 
-    override fun editSendToManyItem(arrayList: ArrayList<BulkyRequestModel>, arrayListJson2: ArrayList<String>) {
-//        TODO("Not yet implemented")
+    override fun editSendToManyItem(updatedItem: MultiContactModel, position: Int) {
+        arrayList.set(position, updatedItem)
+        usersAdapter.setContact(arrayList)
+        var amountTotal = 0
+        for (multiContactModel: MultiContactModel in arrayList) {
+            amountTotal += multiContactModel.amount.toInt()
+        }
+        totalAmount.text = "Kes ${NumberFormat.getNumberInstance(Locale.getDefault()).format(amountTotal)}"
+    }
+
+    override fun updateItem(item: String, itemPosition: String) {
+        val editSendToManyBottomSheet = EditSendToManyBottomSheet();
+        val bundle = Bundle()
+        bundle.putString("personData", item)
+        bundle.putString("itemPosition", itemPosition)
+        editSendToManyBottomSheet.arguments = bundle
+        editSendToManyBottomSheet.show(supportFragmentManager, "Edit Contact");
     }
 }
