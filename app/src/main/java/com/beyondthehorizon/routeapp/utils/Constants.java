45f2fb38540c0d132a87499e0c9c44ac69f22251
= <=>
2020-08-20 06:11:43 +0300

    Update bas url for production readiness

--- app/src/main/java/com/beyondthehorizon/routeapp/utils/Constants.java
+++ app/src/main/java/com/beyondthehorizon/routeapp/utils/Constants.java
@@ -7,7 +7,7 @@ import com.koushikdutta.ion.Ion;
 import com.koushikdutta.ion.future.ResponseFuture;
 
 public class Constants {
-    public static String BASE_URL = "http://164.90.237.6/api/v1/";
+    public static String BASE_URL = "https://route-api.com/api/v1/";
     private static boolean ALLOW_REDIRECT = false;
     public static String REG_APP_PREFERENCES = "profilePref";
     public static String VISITING_HISTORY_PROFILE = "VISITING_HISTORY_PROFILE";
