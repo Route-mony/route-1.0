8119fd92b8a1eac027e5be4810341386757cb44c
PromasterGuru <=>
2021-01-19 15:00:59 +0300

    Implement carousel slider progress indicators

--- lib/pages/services.dart
+++ lib/pages/services.dart
@@ -12,6 +12,7 @@ class Services extends Base {
 class _ServicesState extends State<Services> {
   ApiServices apiServices = ApiServices();
   List<Advert> adverts = List();
+  int pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +25,8 @@ class _ServicesState extends State<Services> {
                     image: AssetImage("assets/images/ss.png"),
                     alignment: Alignment.bottomRight)),
             child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 0.0, horizontal: 10),
-                child: CarouselSlider(
-                  options: CarouselOptions(
+              CarouselSlider(
+                options: CarouselOptions(
                     autoPlay: true,
                     initialPage: 0,
                     height: MediaQuery.of(context).size.height * 0.8,
@@ -36,55 +34,75 @@ class _ServicesState extends State<Services> {
                     autoPlayInterval: Duration(seconds: 5),
                     scrollDirection: Axis.horizontal,
                     viewportFraction: 1,
-                  ),
-                  items: adverts.map((advert) {
-                    return Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                gradient: LinearGradient(
-                                    colors: [
-                                      Base.colors["cardStartColor"],
-                                      Base.colors["cardEndColor"]
-                                    ],
-                                    begin: Alignment(-1.0, 0.0),
-                                    end: Alignment(1.0, 0.0),
-                                    transform: GradientRotation(45))),
-                            child: Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Image(
-                                    image: NetworkImage(advert.url),
-                                    height: 300,
-                                  ),
-                                  Text(
-                                    advert.title,
-                                    style: TextStyle(
-                                        color: Base.colors["colorAccent"],
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
-                                  SizedBox(
-                                    height: 35,
-                                  ),
-                                  Text(
-                                    advert.description,
-                                    style: TextStyle(
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        pageIndex = index;
+                      });
+                    }),
+                items: adverts.map((advert) {
+                  return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: LinearGradient(
+                                  colors: [
+                                    Base.colors["cardStartColor"],
+                                    Base.colors["cardEndColor"]
+                                  ],
+                                  begin: Alignment(-1.0, 0.0),
+                                  end: Alignment(1.0, 0.0),
+                                  transform: GradientRotation(45))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Image(
+                                  image: NetworkImage(advert.url),
+                                  height: 300,
+                                ),
+                                Text(
+                                  advert.title,
+                                  style: TextStyle(
                                       color: Base.colors["colorAccent"],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )));
-                  }).toList(),
-                ),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                SizedBox(
+                                  height: 35,
+                                ),
+                                Text(
+                                  advert.description,
+                                  style: TextStyle(
+                                    color: Base.colors["colorAccent"],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )));
+                }).toList(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: adverts.map((i) {
+                  int index = adverts.indexOf(i);
+                  return Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: pageIndex == index
+                            ? Base.colors["colorPrimaryMain"]
+                            : Base.colors["colorPrimaryLight"]),
+                  );
+                }).toList(),
               ),
               Padding(
-                padding: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.fromLTRB(10.0, 0, 0, 30),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   mainAxisSize: MainAxisSize.max,
